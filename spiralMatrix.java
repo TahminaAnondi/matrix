11ee7bdd698599df463134de57075ecc26b4ad49
TahminaAnondi <tahmina.akter.anondi@gmail.com>
2023-10-09 23:03:47 -0500

    fixed bugs

---
+++ spiralMatrix.java
@@ -0,0 +1,16 @@
+package Matrix;
+
+public class spiralMatrix {
+    public static void printSpiral(int matrix[][]){
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCold = matrix[0].length-1;
+    }
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8,},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+    }
+}
